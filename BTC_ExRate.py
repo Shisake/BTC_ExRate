b1d444986073f55eda0bcc75986acd4298f793e6
Shisake <nesrsta.tom@gmail.com>
2018-01-23 17:38:51 +0100

    Fixed Intendation error

--- BTC_ExRate.py
+++ BTC_ExRate.py
@@ -11,13 +11,14 @@ def getBTCExRate():
     ExRate = data['USD']['last']
     return ExRate
 
-    if sys.argv[1:] == "log":
-        while True:
-            with open("/home/pi/BTC_ExchangeRateOut.txt","a") as f:
-	        now = datetime.datetime.now()
-	        f.write(now.strftime("%d.%m.%y | %H:%M  ") +
-		str(getBTCExRate()) + "\n")
-                time.sleep(60)
-   else:
-        print str(getBTCExRate()) + " $"
-        input()
+if sys.argv[1:] == "log":
+    while True:
+        with open("/home/pi/BTC_ExchangeRateOut.txt","a") as f:
+            now = datetime.datetime.now()
+            f.write(now.strftime("%d.%m.%y | %H:%M  ") +
+            str(getBTCExRate()) + "\n")
+            time.sleep(60)
+else:
+    print str(getBTCExRate()) + " $"
+
+input()
