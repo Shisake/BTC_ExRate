865b4e357909f42d3f62d78fbc10d6bb6396e1af
Shisake <nesrsta.tom@gmail.com>
2018-02-01 15:58:19 +0100

    0

--- BTC_ExRate.py
+++ BTC_ExRate.py
@@ -3,6 +3,7 @@ import json
 import time
 import sys
 import datetime
+from RPLCD.gpio import CharLCD
 
 arguments = ['log', 'display']
 
@@ -21,7 +22,9 @@ while True:
             f.write(now.strftime("%d.%m.%y | %H:%M  ") + str(getBTCExRate()) + "\n")
 
     if "display" in sys.argv[1:]:
-
+        lcd = CharLCD(pin_rs=15, pin_rw=18, pin_e=16, pins_data[21, 22, 23, 24],
+        numbering_mode=GPIO.BOARD)
+        lcd.write_string("Ahoj")
     time.sleep(60)
 else:
     print str(getBTCExRate()) + " $"
